22f8f9686a7cc3c31dd0047bf4d13f413f937923
Aleksandr_Ilin2 <aleksandr_ilin2@epam.com>
2021-11-22 08:48:08 +0300

    first commit

---
+++ src/main/java/gumbert/humbert/bestwatching/BestwatchingApplication.java
@@ -0,0 +1,13 @@
+package gumbert.humbert.bestwatching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestwatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestwatchingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gumbert/humbert/bestwatching/BestwatchingApplicationTests.java
@@ -0,0 +1,13 @@
+package gumbert.humbert.bestwatching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestwatchingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
