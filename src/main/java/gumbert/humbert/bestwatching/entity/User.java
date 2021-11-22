80ce1b41b25b036b3ef3c8da50b3eeb6cc2c0f52
Aleksandr_Ilin2 <aleksandr_ilin2@epam.com>
2021-11-22 09:43:18 +0300

    add userDeatails to user

--- src/main/java/gumbert/humbert/bestwatching/entity/User.java
+++ src/main/java/gumbert/humbert/bestwatching/entity/User.java
@@ -1,18 +1,56 @@
 package gumbert.humbert.bestwatching.entity;
 
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 
 @Entity
 @Table(name = "users")
 @Data
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
     private String password;
-    private boolean isActive;
     private Role role;
+    private boolean isActive;
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return role.getGrantedAuthorities();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return isActive;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return isActive;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return isActive;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive;
+    }
 }
